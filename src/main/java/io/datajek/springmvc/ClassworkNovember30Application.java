991dbc98c9bd3a35f33d9a3161d3dee470743385
BBFerdinandov18 <BBFerdinandov18@codingburgas.bg>
2022-11-30 14:29:43 +0200

    First commit

---
+++ src/main/java/io/datajek/springmvc/ClassworkNovember30Application.java
@@ -0,0 +1,13 @@
+package io.datajek.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassworkNovember30Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassworkNovember30Application.class, args);
+    }
+
+}
