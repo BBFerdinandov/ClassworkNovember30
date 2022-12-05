4df1c1d1dfc3c8333cb7ddfcf260cb1eff3fdda7
BBFerdinandov18 <BBFerdinandov18@codingburgas.bg>
2022-12-05 09:35:41 +0200

    Done with 3-44

--- src/main/java/io/datajek/springmvc/Athlete.java
+++ src/main/java/io/datajek/springmvc/Athlete.java
@@ -1,7 +1,11 @@
 package io.datajek.springmvc;
 
+import org.springframework.web.bind.annotation.InitBinder;
+
 public class Athlete {
     private String firstName;
+    //@NotNull(message="This is a required field.")
+    //@Size(min=1, message = "This is a required field.")
     private String lastName;
     private String country;
 
@@ -28,4 +32,12 @@ public class Athlete {
     public void setCountry(String country) {
         this.country = country;
     }
+
+    /*@InitBinder
+    public void initBinder(WebDataBinder binder) {
+        String TrimmerEditor editor = new StringTrimmerEditor(true);
+        binder.registerCustomEditor(String.class, editor);
+    }*/
+
+
 }
