33cccf906ea6bdcfc49dbf95bfc4efd3fb786228
BBFerdinandov18 <BBFerdinandov18@codingburgas.bg>
2022-12-05 09:47:54 +0200

    Done with 3-49

---
+++ src/main/java/io/datajek/springmvc/PlayerStats.java
@@ -0,0 +1,8 @@
+package io.datajek.springmvc;
+
+public class PlayerStats {
+    private String player1;
+    private String player2;
+    private String head2head;
+
+}
---
+++ src/main/java/io/datajek/springmvc/StatsController.java
@@ -0,0 +1,15 @@
+package io.datajek.springmvc;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*@Controller
+public class StatsController {
+    //method to handle /showStatsForm
+    @RequestMapping("/showStatsForm/")
+    public String showForm(Model model) {
+        model.addAttribute("playerStats", new PlayerStats());
+        return "head-to-head";
+    }
+}*/
