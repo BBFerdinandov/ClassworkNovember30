65c0bac6c1ec962fc63c8a1e7cc24a7b0e499b07
BBFerdinandov18 <BBFerdinandov18@codingburgas.bg>
2022-12-05 09:43:01 +0200

    Done with 3-47

--- src/main/java/io/datajek/springmvc/Athlete.java
+++ src/main/java/io/datajek/springmvc/Athlete.java
@@ -10,6 +10,14 @@ import javax.validation.constraints.Past;
 public class Athlete {
    //@NotNull(message="This is a required field.")
 
+    public String getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    public void setPrizeMoney(String prizeMoney) {
+        this.prizeMoney = prizeMoney;
+    }
+
     @Past(message="Date must be in the past.")
 
     //private Date lastWon;
@@ -18,6 +26,9 @@ public class Athlete {
 
     @Min(value=1, message="Value must be greater than or equal to 1.")
     @Max(value=100, message="Value must be less than or equal to 100")
+
+    //@Pattern(regexp="^\\$([0-9]{1,3},([0-9]{3},)*[0-9]{3})$", message="Incorrect format")
+    private String prizeMoney;
 
     private Integer rank;
     private String firstName;
