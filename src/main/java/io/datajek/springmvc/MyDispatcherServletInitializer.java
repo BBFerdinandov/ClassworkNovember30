a90051481a7c7a100815c7091665268f94fed931
BBFerdinandov18 <BBFerdinandov18@codingburgas.bg>
2022-12-05 09:50:22 +0200

    Done with 3-49

---
+++ src/main/java/io/datajek/springmvc/MyDispatcherServletInitializer.java
@@ -0,0 +1,18 @@
+package io.datajek.springmvc;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/*public class MyDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    //@Override
+    protected Class<?>[] getRootConfigClassse() {
+        return null;
+    }
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override protected String[] getServletMappings() {
+        return null;
+    }
+}*/
